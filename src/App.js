25ffe78c45aa9308bb4f409716b72ad884a42b27
KateLockhart <katelockhart707@gmail.com>
2022-09-30 14:49:54 -0400

    OnClick successfully using all user controller routes and persisting to db, set up for task controller fetch tests in place.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [result, setResult] = useState([]);
+
+  const taskObj = {
+    task: {},
+  };
+
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:3000/task/", {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      // body: JSON.stringify(taskObj),
+    });
+    const data = await res.json();
+    console.log(data);
+    setResult(data);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={fetchData}>Press the Button to Fetch</button>
+    </div>
+  );
 }
 
 export default App;
