87ac428931b64fb8d7c413f8d2e891ff8f66c5a4
KateLockhart <katelockhart707@gmail.com>
2022-10-11 17:36:59 -0400

    Installed Material UI, icons, and styling packages. Added in nav and footer Material placeholders.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home, Matrix, Login, SignUp, About, Contact, Privacy } from "./views";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
 // TODO: Conditionally render home page when not logged in(sign up landing and app info), matrix when logged in; nav with same conditional Login/Sign Up and Logout buttons
 function App() {
   const [result, setResult] = useState([]);
@@ -22,19 +28,21 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul className="App-header">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/matrix">My Priorities</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+        <AppBar position="static">
+          <Toolbar>
+            <Stack direction="row" spacing={3}>
+              <Link to="/">
+                <Button variant="contained">Home</Button>
+              </Link>
+              <Link to="/matrix">
+                <Button variant="contained">My Priorities</Button>
+              </Link>
+              <Link to="/login">
+                <Button variant="contained">Login</Button>
+              </Link>
+            </Stack>
+          </Toolbar>
+        </AppBar>
 
         <Routes>
           <Route path="" element={<Home />}></Route>
@@ -49,17 +57,25 @@ function App() {
         <button onClick={fetchData}>Press the Button to Fetch</button>
 
         <footer>
-          <ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/privacyPolicy">Privacy Policy</Link>
-            </li>
-          </ul>
+          <Toolbar>
+            <Stack direction="row" spacing={50}>
+              <Link to="/about">
+                <Button variant="contained" size="small">
+                  About
+                </Button>
+              </Link>
+              <Link to="/contact">
+                <Button variant="contained" size="small">
+                  Contact
+                </Button>
+              </Link>
+              <Link to="/privacyPolicy">
+                <Button variant="contained" size="small">
+                  Privacy Policy
+                </Button>
+              </Link>
+            </Stack>
+          </Toolbar>
         </footer>
       </div>
     </Router>
