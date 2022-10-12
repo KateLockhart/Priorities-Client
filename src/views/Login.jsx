379ca00e018de82fff89d7b325d59ce983c56545
KateLockhart <katelockhart707@gmail.com>
2022-10-12 15:59:12 -0400

    Created base sign up and login form with Material components.

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,25 @@
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+
 export const Login = () => {
-    // TODO: Conditionally render login vs Sign Up forms
+    // TODO: Implement full Login form build and functionality
     return (
         <div>
-            <h1>This is the Auth Page Login view.</h1>
-            
+            <h1>Login</h1>
+            <Box
+                component="form"
+                sx={{
+                  '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <FormControl>
+                <TextField id="outlined-basic" label="Email" variant="outlined" />
+                <TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
+                </FormControl>
+            </Box>
         </div>
     )
 }
--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,9 +1,51 @@
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import InputLabel from "@mui/material/InputLabel";
+import FormHelperText from '@mui/material/FormHelperText';
+import TextField from "@mui/material/TextField";
+import Checkbox from '@mui/material/Checkbox';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+
 export const SignUp = () => {
     // TODO: Conditionally render login vs Sign Up forms
     return (
-        <div>
-            <h1>This is the Auth Page Sign Up view.</h1>
-            
+        <div id="signUpPage">
+            <h1>Sign Up</h1>
+            <Box
+                component="form"
+                sx={{
+                  '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <FormControl >
+                    <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+                    <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                    <TextField id="outlined-basic" label="Username" variant="outlined" />
+                    <TextField id="outlined-basic" label="Password" type="password"  variant="outlined" />
+                    <TextField id="outlined-basic" label="Confirm Password" type="password"  variant="outlined" />
+                    <FormControlLabel control={<Checkbox color="success"/>} label="I would like to receive notifications of my priorities." />
+                    
+                    {/* Notification Preference drop down */}
+                    <InputLabel id="notification-select-label">Notification Preference</InputLabel>
+                    <Select
+                    labelId="notification-select-label"
+                    id="notification-select"
+                    // value={notificationPreference}
+                    label="Notification Preference"
+                    // onChange={handleChange}
+                    >
+                        <MenuItem value={'email'}>Email</MenuItem>
+                        <MenuItem value={'phone'}>Text Message</MenuItem>
+                    </Select>
+                    
+                    <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+                </FormControl>
+            </Box>
         </div>
     )
 }
