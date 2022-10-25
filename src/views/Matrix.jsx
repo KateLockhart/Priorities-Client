ac4dc3572b46f214b5a470d4b0c8c51eee270d66
KateLockhart <katelockhart707@gmail.com>
2022-10-25 13:49:14 -0400

    Added TODOs and list structure format in matrix.

--- src/views/Matrix.jsx
+++ src/views/Matrix.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // Grid imports
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
@@ -13,39 +14,143 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Checkbox from "@mui/material/Checkbox";
 
+//  TODO: Add "Add Priority" button and link to form page
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 export const Matrix = () => {
+  const [checked, setChecked] = useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  // let priority = () => {
+  //     return (
+  //         <ListItem>
+  //             <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+  //           <ListItemIcon>
+  //             <Checkbox
+  //               edge="start"
+  //               checked={checked.indexOf(value) !== -1}
+  //               tabIndex={-1}
+  //               disableRipple
+  //               inputProps={{ 'aria-labelledby': labelId }}
+  //             />
+  //           </ListItemIcon>
+  //           <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+  //         </ListItemButton>
+  //         </ListItem>
+  //     )
+  // }
+
   return (
-    <div>
-      <h1>This is the Matrix Page view.</h1>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={6}>
-            <Item>
-              <Divider>Important and Urgent</Divider>
+    <div style={{ height: "67.25vh" }}>
+      <h1>Your Priority Matrix</h1>
+      <Box sx={{ flexGrow: 1, padding: "4%" }}>
+        <Grid container spacing={4}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            // sx={{
+            //   outline: "solid 3px #77d6a7",
+            //   borderRadius: "3px",
+            //   outlineOffset: "10px",
+            // }}
+          >
+            <Divider>Important and Urgent</Divider>
+            <Item
+              sx={{
+                margin: "1.5%",
+              }}
+            >
+              {/* To be created by API call */}
+              <List
+                sx={{
+                  width: "100%",
+                  maxWidth: 360,
+                  bgcolor: "background.paper",
+                }}
+              >
+                {[0, 1, 2, 3].map((value) => {
+                  const labelId = `checkbox-list-label-${value}`;
+
+                  return (
+                    <ListItem
+                      key={value}
+                      // secondaryAction={
+                      //   <IconButton edge="end" aria-label="comments">
+                      //     <CommentIcon />
+                      //   </IconButton>
+                      // }
+                      disablePadding
+                    >
+                      <ListItemButton
+                        role={undefined}
+                        onClick={handleToggle(value)}
+                        dense
+                        disableRipple
+                      >
+                        <ListItemIcon>
+                          <Checkbox
+                            edge="start"
+                            checked={checked.indexOf(value) !== -1}
+                            tabIndex={-1}
+                            disableRipple
+                            inputProps={{ "aria-labelledby": labelId }}
+                          />
+                        </ListItemIcon>
+                        <ListItemText
+                          id={labelId}
+                          primary={`Line item ${value + 1}`}
+                        />
+                      </ListItemButton>
+                    </ListItem>
+                  );
+                })}
+              </List>
             </Item>
           </Grid>
           <Grid item xs={12} sm={6} md={6}>
-            <Item>
-              <Divider>Urgent, Less Important</Divider>
-            </Item>
+            <Divider>Urgent, Less Important</Divider>
+            <Item
+              sx={{
+                margin: "1.5%",
+              }}
+            ></Item>
           </Grid>
           <Grid item xs={12} sm={6} md={6}>
-            <Item>
-              <Divider>Important, Less Urgent</Divider>
-            </Item>
+            <Divider>Important, Less Urgent</Divider>
+            <Item
+              sx={{
+                margin: "1.5%",
+              }}
+            ></Item>
           </Grid>
           <Grid item xs={12} sm={6} md={6}>
-            <Item>
-              <Divider>Less Urgent and Important</Divider>
-            </Item>
+            <Divider>Less Urgent and Important</Divider>
+            <Item
+              sx={{
+                margin: "1.5%",
+              }}
+            ></Item>
           </Grid>
         </Grid>
       </Box>
