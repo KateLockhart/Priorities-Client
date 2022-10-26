7efe03af301e293e13f32886b450b1bda2fe84d3
KateLockhart <katelockhart707@gmail.com>
2022-10-26 17:26:58 -0400

    Added TODOs and next steps to privacy policy view.

--- src/views/Privacy.jsx
+++ src/views/Privacy.jsx
@@ -2,6 +2,8 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 
+//  TODO: Add privacy content and create both a summary and in depth section
+
 export const Privacy = () => {
   return (
     <div>
@@ -9,7 +11,9 @@ export const Privacy = () => {
       <Box sx={{ flexGrow: 1, padding: "4%" }}>
         <Grid container>
           <Grid item xs={10} sm={8} md={8}>
-            <Paper elevation="6"></Paper>
+            <Paper elevation="6">
+              {/* Think about using a complex card for summary https://mui.com/material-ui/react-card/#RecipeReviewCard.js, then drop down accordion (https://mui.com/material-ui/react-accordion/#CustomizedAccordions.js) questions with more complex explications */}
+            </Paper>
           </Grid>
         </Grid>
       </Box>
