2ce5b28a63b854f14db9b87bfa2f1d51dc30fa7e
KateLockhart <katelockhart707@gmail.com>
2022-10-27 14:47:22 -0400

    Added grid layout and relevent MUI components to views.

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,46 +1,64 @@
-
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Diversity1RoundedIcon from '@mui/icons-material/Diversity1Rounded';
-import Diversity2RoundedIcon from '@mui/icons-material/Diversity2Rounded';
-import Diversity3RoundedIcon from '@mui/icons-material/Diversity3Rounded';
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Diversity1RoundedIcon from "@mui/icons-material/Diversity1Rounded";
+import Diversity2RoundedIcon from "@mui/icons-material/Diversity2Rounded";
+import Diversity3RoundedIcon from "@mui/icons-material/Diversity3Rounded";
 
 export const About = () => {
-    return (
-        <div>
-            <h2>Future Org about page.</h2>
-
-            <List>
-                <ListItem alignItems='flex-start'>
-                    <ListItemAvatar>
-                        <Avatar>
-                            <Diversity1RoundedIcon/>
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary='Diversity' secondary='We believe in a diverse and welcoming environment where each employee is included in every aspect of who we are as a company and team.'/>
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1, padding: "4%" }}>
+        <Grid container>
+          <Grid item xs={10} sm={5} md={5}>
+            <Paper elevation="6">
+              <List>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar>
+                      <Diversity1RoundedIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Diversity"
+                    secondary="We believe in a diverse and welcoming environment where each employee is included in every aspect of who we are as a company and team."
+                  />
                 </ListItem>
-                <ListItem alignItems='flex-start'>
-                <ListItemAvatar>
-                        <Avatar>
-                            <Diversity2RoundedIcon/>
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary='Collaboration' secondary='Without collaboration and team effort, none of our progress would be possible. Every team member is essential and valued in all we do.'/>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar>
+                      <Diversity2RoundedIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Collaboration"
+                    secondary="Without collaboration and team effort, none of our progress would be possible. Every team member is essential and valued in all we do."
+                  />
                 </ListItem>
-                <ListItem alignItems='flex-start'>
-                <ListItemAvatar>
-                        <Avatar>
-                            <Diversity3RoundedIcon/>
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary='Communication' secondary='No collaboration would be possible without valuing communication: the freedom to express all thoughts and ideas on our projects, work environment, and process. Communication is key to our personal growth, autonomy, and project success from within the team, with customers, and all app users. Communication is key to a positive and balanced work environment for all individuals.'/>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar>
+                      <Diversity3RoundedIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Communication"
+                    secondary="No collaboration would be possible without valuing communication: the freedom to express all thoughts and ideas on our projects, work environment, and process. Communication is key to our personal growth, autonomy, and project success from within the team, with customers, and all app users. Communication is key to a positive and balanced work environment for all individuals."
+                  />
                 </ListItem>
-            </List>
-            
-            
-        </div>
-    )
-}
+              </List>
+            </Paper>
+          </Grid>
+          <Grid item xs={10} sm={5} md={5}>
+            <Paper elevation="6">{/* Why Priorities was created */}</Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+};
--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,4 +1,6 @@
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import InputLabel from "@mui/material/InputLabel";
@@ -13,66 +15,83 @@ export const SignUp = () => {
   // TODO: Conditionally render login vs Sign Up forms
   return (
     <div id="signUpPage">
-      <h1>Sign Up</h1>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <FormControl>
-          <TextField
-            id="outlined-basic"
-            label="Email Address"
-            variant="outlined"
-          />
-          <FormHelperText id="my-helper-text">
-            We'll never share your email.
-          </FormHelperText>
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            type="password"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-basic"
-            label="Confirm Password"
-            type="password"
-            variant="outlined"
-          />
-          <FormControlLabel
-            control={<Checkbox color="success" />}
-            label="I would like to receive notifications of my priorities."
-          />
+      <Box sx={{ flexGrow: 1, padding: "4%" }}>
+        <h1>Sign Up</h1>
+        <Grid container>
+          <Grid item xs={2} sm={3} md={3}>
+            <Paper elevation="6">
+              Sign up to get started on prioritizing your days.
+            </Paper>
+          </Grid>
+          <Grid item xs={10} sm={9} md={9}>
+            <Paper elevation="6">
+              <Box
+                component="form"
+                sx={{
+                  "& .MuiTextField-root": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <FormControl>
+                  <TextField
+                    id="outlined-basic"
+                    label="Email Address"
+                    variant="outlined"
+                  />
+                  <FormHelperText id="my-helper-text">
+                    We'll never share your email.
+                  </FormHelperText>
+                  <TextField
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined"
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="Confirm Password"
+                    type="password"
+                    variant="outlined"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox color="success" />}
+                    label="I would like to receive notifications of my priorities."
+                  />
 
-          {/* Notification Preference drop down */}
-          <InputLabel id="notification-select-label">
-            Notification Preference
-          </InputLabel>
-          <Select
-            labelId="notification-select-label"
-            id="notification-select"
-            // value={notificationPreference}
-            label="Notification Preference"
-            // onChange={handleChange}
-          >
-            <MenuItem value={"email"}>Email</MenuItem>
-            <MenuItem value={"phone"}>Text Message</MenuItem>
-          </Select>
+                  {/* Notification Preference drop down */}
+                  <InputLabel id="notification-select-label">
+                    Notification Preference
+                  </InputLabel>
+                  <Select
+                    labelId="notification-select-label"
+                    id="notification-select"
+                    // value={notificationPreference}
+                    label="Notification Preference"
+                    // onChange={handleChange}
+                  >
+                    <MenuItem value={"email"}>Email</MenuItem>
+                    <MenuItem value={"phone"}>Text Message</MenuItem>
+                  </Select>
 
-          <TextField
-            id="outlined-basic"
-            label="Phone Number"
-            variant="outlined"
-          />
-          <Button variant="outlined" color="success">
-            Sign Up
-          </Button>
-        </FormControl>
+                  <TextField
+                    id="outlined-basic"
+                    label="Phone Number"
+                    variant="outlined"
+                  />
+                  <Button variant="outlined" color="success">
+                    Sign Up
+                  </Button>
+                </FormControl>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
