2ea5844ea382820e56ba6ce95af1aaf1b3a8eef6
KateLockhart <katelockhart707@gmail.com>
2022-11-21 20:53:47 -0500

    Created xs footer menu for mobile view, linking to views still needed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { AppBar, Box, Button, Container, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+} from "@mui/material";
 import {
   Menu,
   MoreHoriz,
@@ -10,90 +18,95 @@ import {
   GridView,
 } from "@mui/icons-material";
 
-function Footer() {
+export const Footer = () => {
   const [open, setOpen] = useState(false);
 
   return (
     // App bar replaced with BottomBar? Bottom bar be Toolbar on med+ and icons on sm & xs
-    <AppBar>
+    <footer>
       {/* Web View Appbar */}
       <Toolbar disableGutters>
-        <Container
-          maxWidth="xl"
-          // sx={{
-          //   position: "relative",
-          //   backgroundColor: "#4d9699",
-          //   height: "8vh",
-          // }}
+        <Box
+          sx={{
+            flexGrow: 1,
+            backgroundColor: "#4d9699",
+            height: "8vh",
+            display: { xs: "none", sm: "flex", md: "flex" },
+            justifyContent: "space-evenly",
+          }}
         >
-          <Box
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/about"
+            onClick={(e) => setOpen(false)}
             sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "flex" },
-              // justifyContent: "space-evenly",
+              my: 2,
+              color: "white",
+              display: "block",
+              fontFamily: "Noto sans",
             }}
           >
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/about"
-              onClick={(e) => setOpen(false)}
-              sx={{
-                my: 2,
-                color: "white",
-                display: "block",
-                fontFamily: "Noto sans",
-              }}
-            >
-              About
-            </Button>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/contact"
-              onClick={(e) => setOpen(false)}
-              sx={{
-                my: 2,
-                color: "white",
-                display: "block",
-                fontFamily: "Noto sans",
-              }}
-            >
-              Contact
-            </Button>
-            <Button
-              size="small"
-              component={RouterLink}
-              to="/privacyPolicy"
-              onClick={(e) => setOpen(false)}
-              sx={{
-                my: 2,
-                color: "white",
-                display: "block",
-                fontFamily: "Noto sans",
-              }}
-            >
-              Privacy Policy
-            </Button>
-            <Button
-              size="small"
-              disableFocusRipple
-              disableElevation
-              sx={{
-                my: 2,
-                color: "white",
-                display: "block",
-                fontFamily: "Noto sans",
-                cursor: "default",
-              }}
-            >
-              © Priorities 2022
-            </Button>
-          </Box>
-        </Container>
-      </Toolbar>
-    </AppBar>
-  );
-}
+            About
+          </Button>
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/contact"
+            onClick={(e) => setOpen(false)}
+            sx={{
+              my: 2,
+              color: "white",
+              display: "block",
+              fontFamily: "Noto sans",
+            }}
+          >
+            Contact
+          </Button>
+          <Button
+            size="small"
+            component={RouterLink}
+            to="/privacyPolicy"
+            onClick={(e) => setOpen(false)}
+            sx={{
+              my: 2,
+              color: "white",
+              display: "block",
+              fontFamily: "Noto sans",
+            }}
+          >
+            Privacy Policy
+          </Button>
+          <Button
+            size="small"
+            disableFocusRipple
+            disableElevation
+            sx={{
+              my: 2,
+              color: "white",
+              display: "block",
+              fontFamily: "Noto sans",
+              cursor: "default",
+            }}
+          >
+            © Priorities 2022
+          </Button>
+        </Box>
 
-export default Footer;
+        <BottomNavigation
+          showLabels
+          sx={{
+            display: { xs: "flex", sm: "none", md: "none" },
+            width: "100%",
+            backgroundColor: "#4d9699",
+            flexDirection: "space-evenly",
+          }}
+        >
+          <BottomNavigationAction label="Matrix" icon={<Dataset />} />
+          <BottomNavigationAction label="Priority" icon={<AddBox />} />
+          <BottomNavigationAction label="" icon={<MoreHoriz />} />
+        </BottomNavigation>
+      </Toolbar>
+    </footer>
+  );
+};
